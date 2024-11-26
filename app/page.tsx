b6afc682bbfc1ead945eea1a8675f3abb1b348b7
Aayush Gupta <aayush.gupta@appventurez.com>
2024-11-26 23:48:03 +0530

    ES Lint and Prettier set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-white font-black">Welcome to Dev Flow</h1>
+    <h1 className="text-3xl font-black text-white">Welcome to Dev Flow</h1>
   );
 };
 
