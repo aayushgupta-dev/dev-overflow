5b61a5c1658f186d3366b6278158697caa5d6c5d
Aayush Gupta <aayush.gupta@appventurez.com>
2024-11-27 13:19:12 +0530

    Tailwind and CSS set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 const Home = () => {
-  return (
-    <h1 className="text-3xl font-black text-white">Welcome to Dev Flow</h1>
-  );
+  return <h1 className="h1-bold">Welcome to Dev Flow</h1>;
 };
 
 export default Home;
