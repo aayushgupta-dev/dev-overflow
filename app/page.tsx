5c4f65947ddcf3a565a5721c237bc84f5bf4f071
Aayush Gupta <aayush.gupta@appventurez.com>
2024-11-27 14:04:46 +0530

    font set up with Inter and Space Frotesk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1 className="h1-bold">Welcome to Dev Flow</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to Dev Flow!</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next JS World!</h1>
+    </>
+  );
 };
 
 export default Home;
